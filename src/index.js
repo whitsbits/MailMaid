02e1d609665701fb53cb9be0057fc8d041e68446
discostew <50593321+slwhitman@users.noreply.github.com>
2021-08-16 18:46:15 -0400

    tighten distribution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import captureFormData from "./captureFormData.js"
+import captureFormData from "./main.js"
 import onHomepage from "./main.js"
 
+/*
 const scriptStart = new Date();
 const inboxCnt = 0;
 let countStart = 0;
 const cache = CacheService.getUserCache();
 let loopBreak = 0;
 const inc = 200; // InBox Iteration Increment
-
+*/
 
 global.captureFormData = captureFormData;
 global.onHomepage = onHomepage;
--- src/main.js
+++ src/main.js
@@ -30,3 +30,34 @@
     builder.addSection(optionsSection);
     return builder.build();
   }
+
+/**
+ * Action for saving user inputs.
+ * @param {Object} e - Event from add-on server
+ * @return {CardService.ActionResponse} result of action
+ */
+
+function captureFormData(e) {
+    const action = 'Purge' //TODO: Make this a choice
+    const searchString = e.formInput.searchString;
+    let days = e.formInput.days;
+  
+    try {
+      let userProperties = PropertiesService.getUserProperties();
+      userProperties.setProperties({
+        'action': action,
+        'searchString': searchString,
+        'days': days
+      });
+    } 
+    catch (e) {
+        return `Error: ${e.toString()}`;
+      }
+      var stuff = userProperties.getProperties();
+        for (var key in stuff) {
+            Logger.log('Key: %s, Value: %s', key, stuff[key]);
+}
+    return `Settings Saved`;
+  }
+  
+  
