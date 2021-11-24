87a689601fab1f778863e31bbd3a207a30e38b7b
Stewart Whitman <stewart.l.whitman@gmail.com>
2021-11-24 08:26:39 -0500

    refactor eMailReport.js

--- src/emailReport.js
+++ src/emailReport.js
@@ -1,35 +1,32 @@
-    function callSendEmail () {
-      sendReportEmail(true);
-    }
-    
-    /**
-     *  Generate a log, then email it to the person who ran the script.
-     * Not currently used
-     * TODO figure out a way to do this in a more user friendly manner.
-    */
+function testSendEmail () {
+  sendReportEmail(["MailMaid had no rules to process your Inbox","Please set up your rules in the app."]);
+  //sendReportEmail(["akward seal","baby shark","cate","doggo","E","face palm"]);
+}
 
-    function sendReportEmail(bool) {
-      var results = reportArr; //["akward seal","baby shark","cate","doggo","E","face palm"];
-      var recipient = Session.getActiveUser().getEmail();
-      var subject = 'MailMaid Results';
-       if (bool === true){        
-        var message = getEmailHTML(results)
-      }else{
-          var message = `MailMaid had no rules to process your Inbox. \n\n Please set your rules in the app`;
-        };
+/**
+ *  Generate a log, then email it to the person who ran the script.
+ * @param {Object} results - Array for the message to be sent in the email.
+ *                           one line per array element
+*/
 
-      MailApp.sendEmail({
-        to: recipient,
-        subject: subject,
-        htmlBody: message
-      });
-      Logger.log (`${user} - Email sent to ${recipient}`)
-    }
+function sendReportEmail(results) {
+  
+  var recipient = Session.getActiveUser().getEmail();
+  var subject = 'MailMaid Results';     
+  var message = getEmailHTML(results)
+
+  MailApp.sendEmail({
+    to: recipient,
+    subject: subject,
+    htmlBody: message
+  });
+  Logger.log (`${user} - Email sent to ${recipient}`)
+}
 
 
 function getEmailHTML(results) {
-  var templ = HtmlService.createTemplateFromFile('src/report-email.html');
-  templ.results = results;
-  var htmlBody = templ.evaluate().getContent();
-  return htmlBody
+var templ = HtmlService.createTemplateFromFile('src/report-email.html');
+templ.results = results;
+var htmlBody = templ.evaluate().getContent();
+return htmlBody
 }
