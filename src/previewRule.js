a24aa6f0bcae516349acd2c62e70b9561ba26f6d
Stewart Whitman <stewart.l.whitman@gmail.com>
2021-12-22 10:57:38 -0500

    initial commit

--- src/previewRule.js
+++ src/previewRule.js
@@ -1,9 +1,11 @@
 
-function buildSearchURL (searchString) {
+function buildSearchURL(e) {
+  var search = e.formInput.search;
+  var days = ` older_than:${e.formInput.days}d`;
   var baseURL = `https://mail.google.com/mail/u/0/#search/`
-  let encoded = encodeURIComponent(searchString);
+  let encoded = encodeURIComponent(search + days);
   let url = baseURL + encoded;
-  Logger.log (url);
-  return url;
+  return CardService.newActionResponseBuilder()
+    .setOpenLink(CardService.newOpenLink().setUrl(url).setOpenAs(CardService.OpenAs.OVERLAY))
+    .build();
 }
-
