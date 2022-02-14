4ceef3f6983cf1725feecce5de67a7a878983058
Stewart Whitman <stewart.l.whitman@gmail.com>
2022-02-14 14:52:55 -0500

    cleanup of uneeded files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import EmailReplyParser from 'erp';
-
-export function read(text) {
-	return EmailReplyParser.read;
-  };
-  
-  export function parse_reply(text, include_signatures) {
-	  return EmailReplyParser.parse_reply;
-  }
