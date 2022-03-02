892978b049eba723102db3de1d548bbeeab9bd6d
Stewart Whitman <stewart.l.whitman@gmail.com>
2022-03-02 10:57:29 -0500

    ADD FEATURES
    Completed download attachment feature
    Added download attachment choice toggle
    Added parse reply choice toggle
    Changes to be committed:
    modified:   src/downloadToDrive.js

--- src/downloadToDrive.js
+++ src/downloadToDrive.js
@@ -1,14 +1,14 @@
 /**
- * TODO: Add option for attachment download
- * TODO: Add option for parsing reply out
  * TODO: Loop & timeout for large search results
  */
 
 function downloadToDrive(search, downloadAction, saveFile, fileTypeAction) {
-    var search = "subject:(Welcome to Rye Country Day School!)"
-    var downloadAction = "Download"
-    var saveFile = "threadFile"
-    var fileTypeAction = "text"
+  var search = "from:anniep519@me.com has:attachment"
+  var downloadAction = "Download"
+  var saveFile = "threadFile"
+  var fileTypeAction = ".txt";
+  var atchDownload = true;
+  var parseReply = false;
 
   Logger.log(`${user} - Processing ${downloadAction} with ${search} as type ${fileTypeAction} to ${saveFile}.`)
   var threads = GmailApp.search(search, 0, inc);
@@ -27,33 +27,53 @@ function downloadToDrive(search, downloadAction, saveFile, fileTypeAction) {
     var thread = '';
     for (var j = 0; j < messages.length; j++) {
       var filename = GmailApp.getMessageById(messages[0].getId()).getSubject();
+      if (filename === ''){
+        filename = "No Message Subject"
+      }
       var msgID = messages[j].getId();
-
       var msg = GmailApp.getMessageById(msgID);
 
 
-      if (fileTypeAction === 'html') {
-        var msgRaw = EmailReplyParser.parse_reply(msg.getBody());
+    if (atchDownload){
+      var attachments = messages[j].getAttachments();
+      for(var k in attachments){
+        var atch = attachments[k];
+        var atchName = atch.getName();
+        var atchSuffix = atchName.substring(atchName.indexOf('.'),atchName.length);
+        var atchFile = atch.copyBlob();
+        saveToFile(atchSuffix, atchFile, filename, newFolder)
+      }
+    }
+      if (fileTypeAction === '.html') {
+        if (parseReply){
+          var msgRaw = EmailReplyParser.parse_reply(msg.getBody());
+        }else{
+          var msgRaw = msg.getBody();
+        }
         var header = `<div class="gmail_default">Date: ${msg.getDate()} <br>From: ${msg.getFrom()} <br>To: ${msg.getTo()} <br>\
  Cc: ${msg.getCc()}<br><br></div>`;
-          if (header.length > 80){
-            header = fold(80, ',','<br>',header);
-          }
-      } else if (fileTypeAction === "eml") {
+      if (header.length > 80) {
+          header = fold(80, ',', '<br>', header);
+        }
+      } else if (fileTypeAction === ".eml") {
         var msgRaw = msg.getRawContent();
-      } else {  //defaul to PlainBody and .txt
-        var msgRaw = EmailReplyParser.parse_reply(msg.getPlainBody());
+        
+      } else {  //default to PlainBody and .txt
+        if (parseReply) {
+          var msgRaw = EmailReplyParser.parse_reply(msg.getPlainBody());
+        }else{
+          var msgRaw = msg.getPlainBody();
+        }
         var header = `Date: ${msg.getDate()} \nFrom: ${msg.getFrom()} \n To: ${msg.getTo()} \n \
 Cc: ${msg.getCc()} \n\n`;
-        if (header.length > 80){
-          header = fold(80,',','\n', header);
+        if (header.length > 80) {
+          header = fold(80, ',', '\n', header);
         }
       };
 
       if (saveFile === 'messageFile') {
         saveToFile(fileTypeAction, msgRaw, filename, newFolder);
       }
-
 
       thread += (`${header}\n${msgRaw}\n${slug}\n`);
     }
@@ -78,20 +98,20 @@ Cc: ${msg.getCc()} \n\n`;
 }
 
 
-function saveToFile(fileTypeAction, file, filename, folder) {
-  if (fileTypeAction === 'html') {
+function saveToFile(suffix, file, filename, folder) {
+  if (suffix === '.html') {
     var msgBlob = Utilities.newBlob(file, 'text/html', 'message.html');
     var file = DriveApp.getFolderById(folder).createFile(msgBlob);
-    file.setName(filename + ".html");
-  } else if (fileTypeAction === "eml") {
+  } else if (suffix === ".eml") {
     var msgBlob = Utilities.newBlob(file, 'message/rfc822', 'message.eml');
     var file = DriveApp.getFolderById(folder).createFile(msgBlob);
-    file.setName(filename + ".eml");
-  } else {
+  } else if (suffix === ".txt") {
     var msgBlob = Utilities.newBlob(file, 'text/plain', 'message.txt');
     var file = DriveApp.getFolderById(folder).createFile(msgBlob);
-    file.setName(filename + ".txt");
+  }else{ //for any attachments
+    var file = DriveApp.getFolderById(folder).createFile(file);
   }
+  file.setName(filename + suffix);  
   return
 }
 
@@ -106,14 +126,16 @@ function printSlug() {
 
 function fold(limit, charBreak, lineBreak, str) {
   let brokenString = '';
-   for(let i = 0, count = 0; i < str.length; i++){
-      if(count >= limit && str[i] === charBreak){
-         count = 0;
-         brokenString += lineBreak;
-      }else{
-         count++;
-         brokenString += str[i];
-      }
-   }
-   return brokenString;
+  for (let i = 0, count = 0; i < str.length; i++) {
+    if (count >= limit && str[i] === charBreak) {
+      count = 0;
+      brokenString += lineBreak;
+    } else {
+      count++;
+      brokenString += str[i];
+    }
+  }
+  return brokenString;
 }
+
+
