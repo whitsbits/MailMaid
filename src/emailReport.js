f0c7796916c79b5291178d50316db31d37cce423
Stewart Whitman <stewart.l.whitman@gmail.com>
2022-05-27 12:21:21 -0400

    Update sendReportEmail to call url for external script so that the email can be sent from a centralized mailbox instead of the individual users mailbox. Purpose was to reduce scope of permissions required to run the application modified:   appsscript.json modified:   src/emailReport.js

--- src/emailReport.js
+++ src/emailReport.js
@@ -1,8 +1,8 @@
 function testSendEmail() {
   sendReportEmail("MailMaid Needs your attention, please", "src/basic-email.html", false, licenseRead(), null,
-       ["For MailMaid to continue to work, you need to launch the app from the right sidebar in your Gmail application and click AUTHORIZE ACCESS",
-       "If you no longer wish to use the application in Trial mode, click the three dots in the upper right of the app, select Manage add-on and then three dots again to Uninstall"])
-    
+    ["For MailMaid to continue to work, you need to launch the app from the right sidebar in your Gmail application and click AUTHORIZE ACCESS",
+      "If you no longer wish to use the application in Trial mode, click the three dots in the upper right of the app, select Manage add-on and then three dots again to Uninstall"])
+
   /*
   cache.getObject('result')
   [
@@ -28,14 +28,19 @@ function sendReportEmail(subject, template, maxMet, licensed, tally, results) {
 
   var recipient = Session.getActiveUser().getEmail();
   var subject = subject;
-  var message = getEmailHTML(template, maxMet, licensed, tally, results)
-
-  MailApp.sendEmail({
-    to: recipient,
-    subject: subject,
-    htmlBody: message
-  });
-  Logger.log(`${user} - Email ${subject} sent to ${recipient}`)
+  var body = getEmailHTML(template, maxMet, licensed, tally, results);
+  let config = {
+    "method": "post",
+    "muteHttpExceptions" : true,
+    "payload": {
+      "recipient": recipient,
+      "subject" : subject,
+      "body": body
+    }
+  };
+  var _url = scriptProperties.getProperty("emailService") //'https://httpbin.org/anything' for testing response
+  var response = UrlFetchApp.fetch(_url, config);
+  Logger.log(`${user} - ${response} - ${subject} sent to ${recipient}`)
 }
 
 /**
