4af9fbf8d06242caff2c9cd83539a749d2f8e747
Stewart Whitman <stewart.l.whitman@gmail.com>
2022-05-31 11:09:25 -0400

    Log bug when no auth to send email from url.fetch

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -99,9 +99,14 @@ function checkAuth() {
   const authInfo = ScriptApp.getAuthorizationInfo(ScriptApp.AuthMode.FULL);
   if (authInfo.getAuthorizationStatus() == ScriptApp.AuthorizationStatus.REQUIRED) {
     Logger.log(`${user} - Missing required scope authorizations`)
+    try{
     sendReportEmail("MailMaid Needs your attention, please", "src/basic-email.html", false, licenseRead(), null,
       ["For MailMaid to continue to work, you need to launch the app from the right sidebar in your Gmail application and click AUTHORIZE ACCESS",
         "If you no longer wish to use the application in Trial mode, click the three dots in the upper right, select Manage add-on and then three dots again to Uninstall"])
+    } catch (e) {
+      Logger.log(`${user} - ${e.toString()}`);
+      Logger.log(`${user} - Can not call UrlFetchApp.fetch to send automated email`)
+    }
     ++checkAuthCount;
     userProperties.setProperty('authCount', checkAuthCount)
     return false
